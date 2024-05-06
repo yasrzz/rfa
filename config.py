bda675b0572ef7c640dc8e3d7c31652affb3c00e
bblkt <169025378+bblkt@users.noreply.github.com>
2024-05-06 20:51:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bblks/AMusic",
+    "https://github.com/bblkt/music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(

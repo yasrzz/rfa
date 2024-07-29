b0df8078fe9fdaa9f889f9cf18a8e538978bf7a9
yasrzz <alwhmy68@gmail.com>
2024-07-29 15:26:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bblkt/music",
+    "https://github.com/yasrzz/rfa",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
